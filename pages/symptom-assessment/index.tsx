dc50d76b516c094d1d6f91800403b74444273dc8
3210jr <3210jr@gmail.com>
2021-01-29 15:52:34 +0300

    fix type issue

--- pages/symptom-assessment/index.tsx
+++ pages/symptom-assessment/index.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import DocsLayout from "../../components/DocsLayout";
 import Footer from "../../components/Footer";
 import SidebarContent from "./components/SidebarContent";
